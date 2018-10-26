8cc8cc829c211fef04334c7e539e2832752c4935
roomylee <roomylee@naver.com>
2018-10-26 18:07:50 +0900

    change pre-trained embeddings from word2vec to glove

--- utils.py
+++ utils.py
@@ -24,27 +24,17 @@ label2class = {0: 'Other',
                17: 'Content-Container(e1,e2)', 18: 'Content-Container(e2,e1)'}
 
 
-def load_word2vec(embedding_path, embedding_dim, vocab):
+def load_glove(embedding_path, embedding_dim, vocab):
     # initial matrix with random uniform
     initW = np.random.randn(len(vocab.vocabulary_), embedding_dim).astype(np.float32) / np.sqrt(len(vocab.vocabulary_))
     # load any vectors from the word2vec
-    print("Load word2vec file {0}".format(embedding_path))
-    with open(embedding_path, "rb") as f:
-        header = f.readline()
-        vocab_size, layer_size = map(int, header.split())
-        binary_len = np.dtype('float32').itemsize * layer_size
-        for line in range(vocab_size):
-            word = []
-            while True:
-                ch = f.read(1).decode('latin-1')
-                if ch == ' ':
-                    word = ''.join(word)
-                    break
-                if ch != '\n':
-                    word.append(ch)
-            idx = vocab.vocabulary_.get(word)
-            if idx != 0:
-                initW[idx] = np.fromstring(f.read(binary_len), dtype='float32')
-            else:
-                f.read(binary_len)
+    print("Load glove file {0}".format(embedding_path))
+    f = open(embedding_path, 'r', encoding='utf8')
+    for line in f:
+        splitLine = line.split(' ')
+        word = splitLine[0]
+        embedding = np.asarray(splitLine[1:], dtype='float32')
+        idx = vocab.vocabulary_.get(word)
+        if idx != 0:
+            initW[idx] = embedding
     return initW
